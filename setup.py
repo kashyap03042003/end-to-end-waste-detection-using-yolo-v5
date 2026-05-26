c30be701589e64b5da192d4d368f43d652b5e3cc
Krishna Kashyap Pasumarthi <kashyappasumarthi@Kashyap.local>
2026-05-25 20:33:54 -0400

    commit 2

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'wasteDetection',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Krishna Kashyap',
+    author_email= 'krishna.kashyap50@gmail.com',
     packages= find_packages(),
     install_requires = []
 
